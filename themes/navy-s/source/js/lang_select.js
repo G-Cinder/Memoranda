8390188b051132a02696729c7f03204f3e3ab7cc
mouguangyi <mouguangyi@ztgame.com>
2020-03-02 12:24:14 +0800

    Update.

--- themes/navy-s/source/js/lang_select.js
+++ themes/navy-s/source/js/lang_select.js
@@ -7,12 +7,8 @@
     var lang = this.value;
     var canonical = this.dataset.canonical;
     var path = this.dataset.root;
-    // if (lang !== 'cn') path += lang + '/';
 
     Cookies.set('nf_lang', lang, { expires: 365 });
     location.href = path + canonical;
   }
-
-  // document.getElementById('lang-select').addEventListener('change', changeLang);
-  // document.getElementById('mobile-lang-select').addEventListener('change', changeLang);
 }());
